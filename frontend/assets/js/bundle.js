65eced579d7ad01411cdd6d43187c9876bf202f9
Matheus Dario <dario.matheus97@gmail.com>
2023-10-30 23:37:52 -0300

    add webpack to project with frontend and backend

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./src/About-webpack/index.ts ***!
+  \************************************/
+
+console.log('Hello there"!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
