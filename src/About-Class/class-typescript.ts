53b1c1b96601c90f7dff4a2f7a7032bc1e657a77
Matheus Dario <dario.matheus97@gmail.com>
2023-11-03 00:44:32 -0300

    about class in typeScript

--- src/About-Class/class-typescript.ts
+++ src/About-Class/class-typescript.ts
@@ -8,8 +8,8 @@ export class Company {
     this.cnpj = cnpj;
   }
 
-  addEmploye(employe: Employe): void {
-    this.employees.push(employe);
+  addEmploye(...employe: Employe[]): void {
+    employe.forEach((employe) => this.employees.push(employe));
   }
 
   showEmployees(): void {
@@ -33,8 +33,6 @@ const company1 = new Company('Google', '11.111.111/0001-01');
 const employe1 = new Employe('Matheus', 'Dario');
 const employe2 = new Employe('Joaquim', 'James');
 const employe3 = new Employe('Joana', 'Costa');
-company1.addEmploye(employe1);
-company1.addEmploye(employe2);
-company1.addEmploye(employe3);
+company1.addEmploye(employe1, employe2, employe3);
 console.log(company1);
 company1.showEmployees();
